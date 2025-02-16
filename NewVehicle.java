a2f7c57a7e542adf11fd980714b0d9fcebf630df
Plo11y <93281770+imploynesais@users.noreply.github.com>
2025-02-16 16:38:26 +0700

    Create NewVehicle.java

---
+++ NewVehicle.java
@@ -0,0 +1,5 @@
+public class NewVehicle extends Vehicle {
+    public NewVehicle(String serialNumber, String name, String model, int year, String manufacturer, double baseCost) {
+        super(serialNumber, name, model, year, manufacturer, baseCost);
+    }
+}
